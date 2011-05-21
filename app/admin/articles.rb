68fcd8e5d99c41cc76e2c7a9ec77686bb3ed3b1d
namakesugi <info@namakesugi.net>
2011-05-22 01:37:55 +0900

    blog article 101

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,19 +1,16 @@
+# coding: utf-8
 ActiveAdmin.register Article do
-## if you use grid
-#  index :as => :grid, :columns => 2 do |article|
-#    div do
-#      span { link_to article.title, admin_article_path(article) }
-#      br
-#      span(:class => 'article_category') { article.category ? link_to(article.category.name, admin_category_path(article.category_id)) : I18n.t(:unknown) }
-#    end
-#  end
-## default is table
+  # filter setting
+  filter :title, :html => "class='hoge'"
+  filter :category, :as => :check_boxes
+  filter :created_at
+
+  # index action
   index do
     column I18n.t('activerecord.attributes.article.title'), :sortable => :title do |f|
       link_to f.title, admin_article_path(f)
     end
     column I18n.t('activerecord.attributes.article.slug'), :slug
-#    column I18n.t('activerecord.attributes.article.category_id'), :category, :sortable => :category_id
     column I18n.t('activerecord.attributes.article.category_id'), :sortable => :category_id do |f|
       f.category ? link_to(f.category.name, admin_category_path(f.category_id)) : I18n.t(:unknown)
     end
@@ -23,6 +20,12 @@ ActiveAdmin.register Article do
       links += link_to I18n.t(:destory_link), admin_article_path(f), :method => :delete, :confirm => I18n.t(:destroy_confirm), :class => "delete_link"
       links
     end
-#    default_actions :name => 'hoge' # if you use default action
   end
+
+  # index and show sidebar
+  sidebar I18n.t(:recent_list), :only => [:index,:show] do
+    render('/admin/articles/recent', :articles => ::Article.find(:all, :limit => 5, :order => 'updated_at DESC'))
+  end
+  # Abbreviation
+#  sidebar :recent
 end
